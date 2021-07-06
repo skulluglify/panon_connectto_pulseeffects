639ee498823cad09f0a3d1c5c60186f043f70b45
skulluglify <skulluglify@outlook.com>
2021-07-06 11:16:10 +0700

    +change/added script injection

--- panon_connectto_pulseeffects.sh
+++ panon_connectto_pulseeffects.sh
@@ -1,7 +1,9 @@
 #! /usr/bin/env bash
 
 function check_binaries () {
+
     FIND_BINARIES=$(which ${1} | grep -Eiv "^${1} not found$")
+
     if [ -z "$FIND_BINARIES" ]; then
         echo -en "\x1b[1;31;40m ${1} not found! \x1b[0m\n" && exit 1
     fi
@@ -13,27 +15,37 @@ check_binaries pulseaudio
 ##? .local/share/plasma/plasmoids/panon
 
 function get_panon_index () {
+
     echo `pacmd list-source-outputs | grep -iB18 'application.name = "panon"' | head -n 1 | awk '{print $2}'`
 }
 
-MAX_TIMEOUT=20
-PULSE_ACTIVATE=0
+function main () {
 
-while true; do
-    if [ $MAX_TIMEOUT -eq 0 ]; then
-        break
-    fi
-    PANON_INDEX=$(get_panon_index)
-    if [ "$PANON_INDEX" ]; then
-        if [ $PULSE_ACTIVATE -eq 0 -o -z "$(pgrep pulseeffects)" ]; then
-            PULSE_ACTIVATE=1 && pulseeffects &
-        fi
-        # sleep 2
-        ERROR_MESSAGE=`pacmd move-source-output $PANON_INDEX PulseEffects_apps.monitor`
-        if [ -z "$(echo $ERROR_MESSAGE | grep -Eiv '(failed to parse|no source found by this name or) index')" ]; then
+    MAX_TIMEOUT=20
+    PULSE_ACTIVATE=0
+
+    while true; do
+        if [ $MAX_TIMEOUT -eq 0 ]; then
             break
         fi
-    fi
-    MAX_TIMEOUT=$(($MAX_TIMEOUT - 1))
-    sleep .5
-done
+        PANON_INDEX=$(get_panon_index)
+        if [ "$PANON_INDEX" ]; then
+            if [ $PULSE_ACTIVATE -eq 0 -o -z "$(pgrep pulseeffects)" ]; then
+                PULSE_ACTIVATE=1 && pulseeffects &
+            fi
+            # sleep 2
+            ERROR_MESSAGE=`pacmd move-source-output $PANON_INDEX PulseEffects_apps.monitor`
+            if [ -z "$(echo $ERROR_MESSAGE | grep -Eiv '(failed to parse|no source found by this name or) index')" ]; then
+                break
+            fi
+        fi
+        MAX_TIMEOUT=$(($MAX_TIMEOUT - 1))
+        sleep .5
+    done
+}
+
+##? MAIN 
+main 
+##? CALLBACK 
+sleep .2
+main 
